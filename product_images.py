e2d8da7c9b7213cdfafe3f03f87745de2d39830b
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2019-08-23 15:10:48 -0300

    add url

--- product_images.py
+++ product_images.py
@@ -13,6 +13,7 @@ class product_image(models.Model):
     image = fields.Binary('Image')
     image_small = fields.Binary('Small Image')
     product_tmpl_id = fields.Many2one('product.template', 'Product')
+    imageurl = fields.Char(string='Image URL')
 
 product_image()
 
